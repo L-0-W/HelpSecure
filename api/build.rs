8168036cbf94f3a9c2b82eb8f7f1329429cc842d
l-0-w <typeint@proton.me>
2026-05-31 07:57:09 -0300

    removendo pasta de API deste projeto em especifico

--- api/build.rs
+++
@@ -1,6 +0,0 @@
-fn main() {
-    println!("cargo:rerun-if-changed=src/polyfill.c");
-    cc::Build::new()
-        .file("src/polyfill.c")
-        .compile("polyfill");
-}
--- api/examples/hash.rs
+++
@@ -1,4 +0,0 @@
-use api::auth::hash_password;
-fn main() {
-    println!("{}", hash_password("teste1234").unwrap());
-}
